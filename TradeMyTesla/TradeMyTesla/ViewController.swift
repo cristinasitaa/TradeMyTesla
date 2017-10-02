ede2b223836689bbbc6df58eae72b3886625c259
Cristina Sita <cristina.sita@evozon.com>
2017-10-02 09:28:08 +0300

    Make predictions

--- TradeMyTesla/TradeMyTesla/ViewController.swift
+++ TradeMyTesla/TradeMyTesla/ViewController.swift
@@ -18,6 +18,9 @@ class ViewController: UIViewController {
     @IBOutlet weak var condition: UISegmentedControl!
     @IBOutlet weak var valuation: UILabel!
     
+    let cars = Cars()
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -25,15 +28,35 @@ class ViewController: UIViewController {
         stackView.setCustomSpacing(30, after: upgrades)
         stackView.setCustomSpacing(30, after: mileage)
         stackView.setCustomSpacing(60, after: condition)
+        calculateValue(self)
         
     }
 
+   
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
-    @IBAction func calculateValue(_ sender: UISegmentedControl) {
+    @IBAction func calculateValue(_ sender: Any) {
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .decimal
+        formatter.maximumFractionDigits = 0
+        let formattedMileage = formatter.string(for: mileage.value) ?? "0"
+        mileageLabel.text = "MILEAGE (\(formattedMileage) miles)"
+        // create a prediction by passing in all its input from our UI
+        if let prediction = try? cars.prediction(model: Double(model.selectedSegmentIndex), premium: Double(upgrades.selectedSegmentIndex), mileage: Double(mileage.value), condition: Double(condition.selectedSegmentIndex)) {
+            // clamp the price so it's at least $2000
+            let clampedValuation = max(2000, prediction.price)
+            formatter.numberStyle = .currency
+            
+            // display the valuation
+            valuation.text = formatter.string(for: clampedValuation)
+        } else {
+            // something went wrong!
+            valuation.text = "Error"
+        }
+        
     }
     
 }
